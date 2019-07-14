f953be057497b7cb75f5b2f52d6a28cf537d6962
youssef samih <youssef.samih97@gmail.com>
2019-07-14 21:06:04 +0100

    add userPhoto method

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { userById, allUsers, getUser, updateUser, deleteUser } = require("../controllers/user")
+const { userById, allUsers, getUser, updateUser, deleteUser, userPhoto } = require("../controllers/user")
 const { requireSingin } = require("../controllers/auth")
 
 const router = express.Router()
@@ -8,6 +8,7 @@ router.get("/users", allUsers)
 router.get("/user/:userId", requireSingin, getUser)
 router.put("/user/:userId", requireSingin, updateUser)
 router.delete("/user/:userId", requireSingin, deleteUser)
+router.get("/user/photo/:userId", userPhoto)
 
 router.param("userId", userById)
 
