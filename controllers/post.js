b6437e6bd2fd7f039ee8783795df2a3f20b24a0e
youssef samih <youssef.samih97@gmail.com>
2019-07-23 00:25:48 +0100

    pagination system

--- controllers/post.js
+++ controllers/post.js
@@ -20,8 +20,14 @@ exports.postById = (req, res, next, id) => {
         })
 }
 
-exports.getPosts = (req, res) => {
-    const posts = Post.find().populate("comments", "text created").populate("comments.postedBy", "_id name").select("_id title body created likes").sort({created: -1}).then(posts => {res.json(posts)}).catch(err => console.log(err))
+exports.getPosts = async (req, res) => {
+    const currentPage = req.query.page || 1
+    const perPage = 3
+    let totalItems;
+    const posts = await Post.find().countDocuments().then(count => {
+        totalItems = count
+        return Post.find().skip((currentPage - 1) * perPage).populate("comments", "text created").populate("comments.postedBy", "_id name").populate("postedBy", "_id name").sort({ date: -1 }).limit(perPage).select("_id title body created likes")
+    }).then(posts => {res.status(200).json(posts)}).catch(err => console.log(err))
 }
 
 exports.createPost = (req, res) => {
